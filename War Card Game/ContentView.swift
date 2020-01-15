8826f3a325bc98cb81dedeccebf9b41136551bfb
Kyle <kylesaldana5@gmail.com>
2020-01-15 11:00:55 -0600

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Kyle Saldana on 1/15/20.
+//  Copyright © 2020 Kyle Saldana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
